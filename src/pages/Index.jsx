b2f628f96a10b0f07cc7f18896c855fe97060a8c
Francisco Alessandro da Silva Soares <alessandrosoares1409@gmail.com>
2025-06-22 15:12:15 -0300

    Finalização do layout e response básica

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -11,21 +11,23 @@ export const Index = () => {
 
             <div className="container-main">
 
-                <h1 className='main-title'>Vamos começar!</h1>
+                <div className="container-text-content">
 
-                <div className="container-content">
+                    <h1 className='main-title'>Vamos começar!</h1>
 
                     <div className="container-main-Input-text">
 
-                        <label htmlFor="input-element">Escreva uma oração na forma padrão da lingua portuguesa e faremos a análise sintática.</label>
+                        <label htmlFor="input-element" className='label-input-element'>Escreva uma oração na forma padrão da lingua portuguesa e faremos a análise sintática.</label>
 
-                        <input type="text" name="input-element" className="input-element" />
+                        <input type="text" name="input-element" className="input-element" placeholder='Ex: Joana chegou tarde na escola...' />
+
+                        <button className='button-to-analise'>Analizar</button>
 
                     </div>
 
-                    <img src="./src/assets/indexImage.svg" className="indexImage" alt="svg-image" />
-
                 </div>
+
+                <img src="./src/assets/indexImage.svg" className="indexImage" alt="svg-image" />
 
             </div>
 
